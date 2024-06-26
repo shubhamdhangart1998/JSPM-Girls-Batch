131fefaf6e1ebceb21b49e26c678b1e12557ab3f
HP <HP@LAPTOP-REUQ2DKO>
2024-06-26 13:15:54 +0530

    Day one

---
+++ DayOne/src/com/tnsif/dayone/IdentiferDemo.java
@@ -0,0 +1,30 @@
+package com.tnsif.dayone;
+
+public class IdentiferDemo {
+
+	public static void main(String[] args) {
+
+		
+		//varibles declaration - Invalid Identifier Examples 
+		
+		//int for=13;// error bcoz keyword cannot be identifier 
+
+		//int num 1=13;  // error bcoz do not add space 
+		
+		//int @num3=13;  //error bcoz identifier can not start with specila symbol except $_
+		
+		//valid Identifier Examples 
+		
+		int $num1= 20;
+		
+		System.out.println("Values of the number varible is :"+ $num1);
+		
+		String studentName="Shreya";
+		
+		System.out.println("value is "+ studentName);
+		
+		
+		
+	}
+
+}
---
+++ DayOne/src/com/tnsif/dayone/TypeCasting.java
@@ -0,0 +1,42 @@
+package com.tnsif.dayone;
+
+public class TypeCasting {
+
+	public static void main(String[] args) {
+
+		//widening  / implicit type casting
+		
+		byte b= 10;
+		int l= b;
+		System.out.println(l);
+		
+		float f= 22.14f;
+		double d= f;
+		System.out.println(d);
+		
+		char ch='A';
+		int i= ch;
+		
+		char var1='\u00A7';
+		System.out.println(var1);
+		int i4=var1;
+		System.out.println(i4);
+		
+		//narrowing /explicit type casting
+		
+		double f1= 10.52f;
+		
+		long l1 = (long) f1;
+		System.out.println(l1);
+		
+		long L2= 98654323456789l;
+		
+		int i23=(int) L2;
+		System.out.println(i23);
+		
+		float f2 = 34.56f;
+		int i123=(int) f2;
+		System.out.println(i123);
+	}
+
+}
