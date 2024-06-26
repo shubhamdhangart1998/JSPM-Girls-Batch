c33007f4224f1c1ba75c589eba0a9c819701e365
HP <HP@LAPTOP-REUQ2DKO>
2024-06-26 13:16:33 +0530

    Day Two

---
+++ DayTwo/src/com/tnsif/daytwo/IfelseDemo.java
@@ -0,0 +1,21 @@
+package com.tnsif.daytwo;
+
+public class IfelseDemo {
+
+	public static void main(String[] args) {
+
+		int age = 200;
+		
+		if (age >= 18)
+		{
+			System.out.println("You are eligible for voting ");
+			
+		}
+		else {
+			System.out.println("sorry ! You need to wait more ");
+		}
+		
+		
+	}
+
+}
