c05644b3f2ce9c4b26da712c9f8a9358d5c80269
HP <HP@LAPTOP-REUQ2DKO>
2024-07-09 12:31:23 +0530

    day 8

---
+++ DayEight/src/com/tnsif/dayeight/abstractclassdemo/Shape.java
@@ -0,0 +1,16 @@
+package com.tnsif.dayeight.abstractclassdemo;
+
+public abstract class Shape {
+	
+	protected float area;
+	
+	//abstarct method
+	public abstract void calArea();
+	
+	//solid method
+	public void show()
+	{
+		System.out.println("Area of the shape is "+ area);
+	}
+
+}
---
+++ DayEight/src/com/tnsif/dayeight/abstractclassdemo/ShapeDemoMain.java
@@ -0,0 +1,30 @@
+package com.tnsif.dayeight.abstractclassdemo;
+
+public class ShapeDemoMain {
+
+	public static void main(String[] args) {
+
+		Square s=new Square();
+		s.calArea();
+		s.show();
+		
+		Rectangular r=new Rectangular();
+		r.calArea();
+		r.show();
+		
+		
+		//dynamic binding //runtime poly //late binding 
+		
+		Shape s1;
+		
+		s1=new Square(5.0f);
+		s1.calArea();
+		s1.show();
+		
+		s1=new Rectangular(10.6f, 7.5f);
+		s1.calArea();
+		s1.show();
+		
+	}
+
+}
